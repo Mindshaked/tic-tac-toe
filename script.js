965c8f56f2a9be6f1c81e271396311c1269f3eae
Mindshaked <eduardgb92@gmail.com>
2022-04-03 16:45:36 +0200

    changed players to objects and message now shows who won

--- script.js
+++ script.js
@@ -8,10 +8,51 @@ const gameBoard =
 " "," "," "];
 
 let won = false;
+let winSymbol = "";
+
+class Player {
+    constructor(){
+        this.pName = "Player";
+        this.wins = 0;
+        this.symbol = '';
+    }
+
+    getName(){
+        return this.pName;
+    }
+
+    setName(player1name){
+        this.pName = player1name;
+    }
+
+    getSymbol(){
+        return this.symbol;
+    }
+
+    setSymbol(sym){
+        this.symbol = sym;
+    }
+    
+    
+}
+
+
+const Player1 = new Player();
+const Player2 = new Player();
+
+Player1.setSymbol("X");
+Player2.setSymbol("O");
 
 const gameFlow = (() => {
 
-    
+    const whatSymbol = (symbol) => {
+        if (symbol == "X"){
+        winSymbol = "X";
+
+    } else if (symbol == "O"){
+        winSymbol = "O";
+    }
+}
 
     const tieResult = (player) => {
         alert("tie")
@@ -19,39 +60,64 @@ const gameFlow = (() => {
 
     const gameCondition = (value) => {
             if (gameBoard[0] == value && gameBoard[1] == value && gameBoard[2] == value){
+                whatSymbol(value);
                 return true; 
             } else if (gameBoard[0] == value && gameBoard[1] == value && gameBoard[2] == value){ //horizontal top
+                whatSymbol(value);
                 return true;
             } else if (gameBoard[3] == value && gameBoard[4] == value && gameBoard[5] == value){//horizontal medio
+                whatSymbol(value);
                 return true;
             } else if (gameBoard[6] == value && gameBoard[7] == value && gameBoard[8] == value){ // horizontal bottom
+                whatSymbol(value);
                 return true;
             } else if (gameBoard[0] == value && gameBoard[3] == value && gameBoard[6] == value){ //vertical izquierda
+                whatSymbol(value);
                 return true;
             } else if (gameBoard[1] == value && gameBoard[4] == value && gameBoard[7] == value){ //vertical centro
+                whatSymbol(value);
                 return true;
             } else if (gameBoard[2] == value && gameBoard[5] == value && gameBoard[8] == value){ // vertical derecha
+                whatSymbol(value);
                 return true;
             } else if (gameBoard[0] == value && gameBoard[4] == value && gameBoard[8] == value){ // diagonal hacia abajo a la derecha
+                whatSymbol(value);
                 return true;
             } else if (gameBoard[2] == value && gameBoard[4] == value && gameBoard[6] == value){ // diagonal hacia arriba a la derecha
+                whatSymbol(value);
                 return true;
             } else if (gameBoard.indexOf(" ") == -1){
                 tieResult();
             }
 
+        
+
         }
     
-     
+    
 
     const gameWon = (value) => {
         if (value == true){
-            alert("has won the game");
             won = true;
-
+            let s = winSymbol;
+            counter = 1;
+            whoWon(s);
+            
         }
     }
 
+    const whoWon = (s) => {
+        if (Player1.getSymbol() == s){
+            Player1.wins++;
+            alert(Player1.getName() + " has won the game");
+            console.log(Player1.wins)
+        } else if (Player2.getSymbol() == s){
+            Player2.wins++;
+            alert(Player2.getName() + " has won the game");
+            console.log(Player2.wins);
+
+        }
+    }
    
 
     
@@ -108,6 +174,8 @@ const gameFlow = (() => {
                     cell[i].innerHTML = gameBoard[i];
                     
             }
+
+            won = false;
         }
     
     
@@ -133,33 +201,31 @@ const playerDisplay = (() => {
 
     player1Form.addEventListener("submit", (e) => {
 
-        let player1name = player1Form['player1name'].value;
-        player1NameDisplay.innerHTML = player1name;
+        Player1.setName(player1Form['player1name'].value);
+        player1NameDisplay.innerHTML = Player1.getName();
         e.preventDefault();
+        return player1name;
         
     });
 
     player2Form.addEventListener("submit", (e) => {
 
-        let player2name = player2Form['player2name'].value;
-        player2NameDisplay.innerHTML = player2name;
+        Player2.setName(player2Form['player2name'].value);
+        player2NameDisplay.innerHTML = Player2.getName();
         e.preventDefault();
+        return player2name;
         
     });
 
-    return {player1name, player2name};
+    
 })();
 
 
-/*
-    const Player1 = (player1name) => {
-        
-        let name = player1name;
-        let wins = 0;
+
     
-        
+
     
-    }
+    /*
 
     const Player2 = (player2name) => {
         const getName = () => name;
